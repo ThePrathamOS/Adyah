f6dd11733b2d107acbf4686f0ab59eee91842179
PrathamOS <38874193+ThePrathamOS@users.noreply.github.com>
2018-06-03 17:39:42 +0530

    Update Updates.sh

--- 1.1/Updates.sh
+++ 1.1/Updates.sh
@@ -24,19 +24,19 @@ then
 		sudo rm -f UnSynced.7z	
 
 		cd /opt/essentials/appimages
-		mkdir temp
+		sudo mkdir temp
 		cd temp
-		mv /opt/UnSyncedUpdate/UnSynced/draw.io-x86_64-8.6.5.AppImage .
+		sudo mv /opt/UnSyncedUpdate/UnSynced/draw.io-x86_64-8.6.5.AppImage .
 		cd ~
 
 		cd /opt/essentials/libreoffice
-		mkdir temp
+		sudo mkdir temp
 		cd temp
-		mv /opt/UnSyncedUpdate/UnSynced/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage .
+		sudo mv /opt/UnSyncedUpdate/UnSynced/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage .
 		cd ~
 
 		cd /opt/UnSyncedUpdate
-		touch FILES
+		sudo touch FILES
 
 		sudo rm -Rf /opt/essentials/calibre
 		sudo mkdir -p /opt/essentials/calibre/calibre
@@ -81,7 +81,7 @@ then
 		then
 			echo -n "1" >> FILES
 		else
-			rm -rf /opt/essentials/appimages/temp
+			sudo rm -rf /opt/essentials/appimages/temp
 		fi
 		
 		FILE="/opt/essentials/libreoffice/temp/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage"
@@ -89,43 +89,46 @@ then
 		then
 			echo -n "2" >> FILES
 		else
-			rm -rf /opt/essentials/libreoffice/temp
+			sudo rm -rf /opt/essentials/libreoffice/temp
 		fi
 
 		read -r TOTAL</opt/UnSyncedUpdate/FILES
-		rm -f FILES
+		sudo rm -f FILES
 
 		if [ "$TOTAL" = "12" ]; then
-			rm -f /opt/essentials/appimages/draw.io*
-			mv /opt/essentials/appimages/temp/draw.io-x86_64-8.6.5.AppImage /opt/essentials/appimages
-			rm -rf /opt/essentials/appimages/temp
-			sed -i '4 s/^/#/' /opt/essentials/appimages/drawio.sh
+			sudo rm -f /opt/essentials/appimages/draw.io*
+			sudo mv /opt/essentials/appimages/temp/draw.io-x86_64-8.6.5.AppImage /opt/essentials/appimages
+			sudo rm -rf /opt/essentials/appimages/temp
+			sudo sed -i '4 s/^/#/' /opt/essentials/appimages/drawio.sh
 			sudo chmod 777 /opt/essentials/appimages/draw.io-x86_64-8.6.5.AppImage
-			echo -e "/opt/essentials/appimages/draw.io-x86_64-8.6.5.AppImage" >> /opt/essentials/appimages/drawio.sh
+			sudo echo -e "/opt/essentials/appimages/draw.io-x86_64-8.6.5.AppImage" >> /opt/essentials/appimages/drawio.sh
 			sed -i '73s~.*~<tr><td>Draw.io</td><td>8.6.5</td><td><a href="https://github.com/jgraph/drawio-desktop/releases" target=_blank>https://github.com/jgraph/drawio-desktop/releases</a></td></tr>~' /opt/essentials/unsyncedupdates.html
 
-			rm -f /opt/essentials/libreoffice/LibreOffice*
-			mv /opt/essentials/libreoffice/temp/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage /opt/essentials/libreoffice
-			rm -rf /opt/essentials/libreoffice/temp
+			sudo rm -f /opt/essentials/libreoffice/LibreOffice*
+			sudo mv /opt/essentials/libreoffice/temp/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage /opt/essentials/libreoffice
+			sudo rm -rf /opt/essentials/libreoffice/temp
 			sudo chmod 777 /opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage
-			sed -i '4 s/^/#/' /opt/essentials/libreoffice/base.sh
-			sed -i '4 s/^/#/' /opt/essentials/libreoffice/calc.sh
-			sed -i '4 s/^/#/' /opt/essentials/libreoffice/draw.sh
-			sed -i '4 s/^/#/' /opt/essentials/libreoffice/impress.sh
-			sed -i '4 s/^/#/' /opt/essentials/libreoffice/math.sh
-			sed -i '4 s/^/#/' /opt/essentials/libreoffice/normal.sh
-			sed -i '4 s/^/#/' /opt/essentials/libreoffice/writer.sh
-			echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --base" >> /opt/essentials/libreoffice/base.sh
-			echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --calc" >> /opt/essentials/libreoffice/calc.sh
-			echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --draw" >> /opt/essentials/libreoffice/draw.sh
-			echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --impress" >> /opt/essentials/libreoffice/impress.sh
-			echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --math" >> /opt/essentials/libreoffice/math.sh
-			echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage" >> /opt/essentials/libreoffice/normal.sh
-			echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --writer" >> /opt/essentials/libreoffice/writer.sh
+			sudo sed -i '4 s/^/#/' /opt/essentials/libreoffice/base.sh
+			sudo sed -i '4 s/^/#/' /opt/essentials/libreoffice/calc.sh
+			sudo sed -i '4 s/^/#/' /opt/essentials/libreoffice/draw.sh
+			sudo sed -i '4 s/^/#/' /opt/essentials/libreoffice/impress.sh
+			sudo sed -i '4 s/^/#/' /opt/essentials/libreoffice/math.sh
+			sudo sed -i '4 s/^/#/' /opt/essentials/libreoffice/normal.sh
+			sudo sed -i '4 s/^/#/' /opt/essentials/libreoffice/writer.sh
+			sudo echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --base" >> /opt/essentials/libreoffice/base.sh
+			sudo echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --calc" >> /opt/essentials/libreoffice/calc.sh
+			sudo echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --draw" >> /opt/essentials/libreoffice/draw.sh
+			sudo echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --impress" >> /opt/essentials/libreoffice/impress.sh
+			sudo echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --math" >> /opt/essentials/libreoffice/math.sh
+			sudo echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage" >> /opt/essentials/libreoffice/normal.sh
+			sudo echo -e "/opt/essentials/libreoffice/LibreOfficeDev-6.2.0.0.alpha0_2018-06-01-x86_64.AppImage --writer" >> /opt/essentials/libreoffice/writer.sh
 			sed -i '83s~.*~<tr><td>LibreOffice</td><td>6.2.0.0.alpha0</td><td><a href="https://libreoffice.soluzioniopen.com/" target=_blank>https://libreoffice.soluzioniopen.com/</a></td></tr>~' /opt/essentials/unsyncedupdates.html
 
-			rm -Rf /opt/UnSyncedUpdate
-			rm -f /opt/POSUPDATE
+			sudo chmod 777 -R /opt/essentials/libreoffice
+			sudo chmod 777 -R /opt/essentials/appimages
+
+			sudo rm -Rf /opt/UnSyncedUpdate
+			sudo rm -f /opt/POSUPDATE
 			sudo -H -u root bash -c 'echo "1.1" | tee /etc/apt/ver'
 
 			sudo kill $(ps -ef | grep bash | grep Notification | awk '{print $2}')
@@ -144,8 +147,8 @@ then
 			mpv /opt/anapmi/norepo.mp3
 			xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
 			echo "Unable To Connect Repository.Please Try Again Later."
-			rm -Rf /opt/UnSyncedUpdate
-			rm -f /opt/POSUPDATE
+			sudo rm -Rf /opt/UnSyncedUpdate
+			sudo rm -f /opt/POSUPDATE
 			exit
 		fi	
 	else
@@ -154,8 +157,8 @@ then
 		mpv /opt/anapmi/norepo.mp3
 		xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
 		echo "Unable To Connect Repository.Please Try Again Later."
-		rm -Rf /opt/UnSyncedUpdate
-		rm -f /opt/POSUPDATE
+		sudo rm -Rf /opt/UnSyncedUpdate
+		sudo rm -f /opt/POSUPDATE
 		exit
 	fi
 else
@@ -164,7 +167,7 @@ else
 	mpv /opt/anapmi/norepo.mp3
 	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
 	echo "Unable To Connect Repository.Please Try Again Later."
-	rm -Rf /opt/UnSyncedUpdate
-	rm -f /opt/POSUPDATE
+	sudo rm -Rf /opt/UnSyncedUpdate
+	sudo rm -f /opt/POSUPDATE
 	exit
 fi
