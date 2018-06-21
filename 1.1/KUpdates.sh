0504ef617857bb3da678714c1a863bd924050413
PrathamOS <38874193+ThePrathamOS@users.noreply.github.com>
2018-06-21 12:57:18 +0530

    Update KUpdates.sh

--- 1.1/KUpdates.sh
+++ 1.1/KUpdates.sh
@@ -1,130 +1,50 @@
 #!/bin/bash
 
-function Chunk1(){
-	wget https://sourceforge.net/projects/getprathamos/files/Adyah/1.1/Kernelaa
-	FILE="Kernelaa"
-	FILESUCCESS="NO"
-	if [ -f "$FILE" ]
-	then
-		FILESUCCESS="YES"
-	else
-		wget https://github.com/ThePrathamOS/Adyah/raw/master/1.1/Kernelaa
-		if [ -f "$FILE" ]
-		then
-			FILESUCCESS="YES"
-		fi
-		if [ "$FILESUCCESS" == "NO" ] || [ "$FILESUCCESS" == "NO" ] ; then
-			echo "Unable To Connect Repository.Please Try Again Later."
-			exit
-		fi
-	fi		
-}
-function Chunk2(){
-	wget https://sourceforge.net/projects/getprathamos/files/Adyah/1.1/Kernelab
-	FILE="Kernelab"
-	FILESUCCESS="NO"
-	if [ -f "$FILE" ]
-	then
-		FILESUCCESS="YES"
-	else
-		wget https://github.com/ThePrathamOS/Adyah/raw/master/1.1/Kernelab
-		if [ -f "$FILE" ]
-		then
-			FILESUCCESS="YES"
-		fi
-		if [ "$FILESUCCESS" == "NO" ] || [ "$FILESUCCESS" == "NO" ] ; then
-			echo "Unable To Connect Repository.Please Try Again Later."
-			exit
-		fi
-	fi		
-}
-function Chunk3(){
-	wget https://sourceforge.net/projects/getprathamos/files/Adyah/1.1/Kernelac
-	FILE="Kernelac"
-	FILESUCCESS="NO"
-	if [ -f "$FILE" ]
-	then
-		FILESUCCESS="YES"
-	else
-		wget https://github.com/ThePrathamOS/Adyah/raw/master/1.1/Kernelac
-		if [ -f "$FILE" ]
-		then
-			FILESUCCESS="YES"
-		fi
-		if [ "$FILESUCCESS" == "NO" ] || [ "$FILESUCCESS" == "NO" ] ; then
-			echo "Unable To Connect Repository.Please Try Again Later."
-			exit
-		fi
-	fi		
-}
-function Chunk4(){
-	wget https://sourceforge.net/projects/getprathamos/files/Adyah/1.1/Kernelad
-	FILE="Kernelad"
-	FILESUCCESS="NO"
-	if [ -f "$FILE" ]
-	then
-		FILESUCCESS="YES"
-	else
-		wget https://github.com/ThePrathamOS/Adyah/raw/master/1.1/Kernelad
-		if [ -f "$FILE" ]
-		then
-			FILESUCCESS="YES"
-		fi
-		if [ "$FILESUCCESS" == "NO" ] || [ "$FILESUCCESS" == "NO" ] ; then
-			echo "Unable To Connect Repository.Please Try Again Later."
-			exit
-		fi
-	fi		
-}
-
-export -f Chunk1 && export -f Chunk2 && export -f Chunk3 && export -f Chunk4
-parallel -j 4 ::: Chunk1 Chunk2 Chunk3 Chunk4
-touch FILES
-FILE="Kernelaa"
+axel https://sourceforge.net/projects/getprathamos/files/Adyah/1.1/Kernel.7z
+FILE="/opt/KernelUpdate/Kernel.7z"
 if [ -f "$FILE" ]
 then
-	echo -n "1" >> FILES
-fi
-FILE="Kernelaa"
-if [ -f "$FILE" ]
-then
-	echo -n "2" >> FILES
-fi
-FILE="Kernelaa"
-if [ -f "$FILE" ]
-then
-	echo -n "3" >> FILES
-fi
-FILE="Kernelaa"
-if [ -f "$FILE" ]
-then
-	echo -n "4" >> FILES
-fi
-read -r TOTAL</opt/KernelUpdate/FILES
-rm -f FILES
-if [ "$TOTAL" = "1234" ]; then
-	cat Kernel* > Kernel.tar.xz
-	tar xf Kernel.tar.xz
-	cd Kernel
-	ls -l
-	notify-send -t 5000 "PrathamOS Kernel Update" "\nInstalling Modules & Headers..."
-	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /opt/anapmi/AI.png	
-	mpv /opt/anapmi/modheaders.mp3
-	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
-	echo "Installing Modules & Headers..."
-	sudo gdebi -n linux-modules-4.16.16-041616-generic_4.16.16-041616.201806160515_amd64.deb
-	sudo gdebi -n linux-headers-4.16.16-041616_4.16.16-041616.201806160515_all.deb
-	sudo gdebi -n linux-headers-4.16.16-041616-generic_4.16.16-041616.201806160515_amd64.deb
-	sudo gdebi -n linux-image-unsigned-4.16.16-041616-generic_4.16.16-041616.201806160515_amd64.deb
-	CurrentUser=$(whoami)
-	CurrentKernelFull=$(uname -r)
-	CurrentKernel=$(uname -r | sed s.-generic.''.g)	
-echo "#!/bin/bash
+	md5sum=$(md5sum $FILE | awk '{print $1}')
+	if [ "$md5sum" = "73fb0cf3692eba6504bf12d98966fcec" ]; then
+		TOTAL="1234"
+		if [ "$TOTAL" = "1234" ]; then
+			sudo 7z x Kernel.7z -o.
+			sudo rm -f Kernel.7z
+			cd Kernel
+			sudo mkdir -p /opt/drivers
+			sudo rm -Rf /opt/drivers/rtl8188eu
+			sudo cp -R rtl8188eu /opt/drivers
+			cd rtl8188eu
+			make
+			sudo make install
+			sudo modprobe 8188eu
+			cd ..
+			notify-send -t 5000 "PrathamOS Kernel Update" "\nInstalling Modules & Headers..."
+			xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /opt/anapmi/AI.png	
+			mpv /opt/anapmi/modheaders.mp3
+			xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
+			echo "Installing Modules & Headers..."
+			KERNELNAME="4.18.0-041800rc1-generic"
+			sudo gdebi -n linux-modules-4.18.0-041800rc1-generic_4.18.0-041800rc1.201806162031_amd64.deb
+			sudo gdebi -n linux-headers-4.18.0-041800rc1_4.18.0-041800rc1.201806162031_all.deb
+			sudo gdebi -n linux-headers-4.18.0-041800rc1-generic_4.18.0-041800rc1.201806162031_amd64.deb
+			sudo gdebi -n linux-image-unsigned-4.18.0-041800rc1-generic_4.18.0-041800rc1.201806162031_amd64.deb
+			CurrentUser=$(whoami)
+			CurrentKernelFull=$(uname -r)
+			CurrentKernel=$(uname -r | sed s.-generic.''.g)	
+			echo "#!/bin/bash
 
 notify-send -t 5000 \"PrathamOS Kernel Update\" \"\nRemoving Kernel $CurrentKernelFull...\"
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /opt/anapmi/AI.png	
 mpv /opt/anapmi/oldkernel.mp3
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
+sudo cp -R /opt/drivers/rtl8188eu /opt/drivers/rtl8188eutemp
+pushd /opt/drivers/rtl8188eutemp
+make
+sudo make install
+sudo modprobe 8188eu
+popd
+sudo rm -Rf /opt/drivers/rtl8188eutemp
 echo \"Removing Kernel $CurrentKernelFull...\"
 sudo apt-get remove -y linux-headers-$CurrentKernel
 sudo apt-get remove -y linux-headers-$CurrentKernelFull
@@ -151,7 +71,7 @@ sudo rm -f /*.old
 rm -f /home/$CurrentUser/.config/autostart/KernelPostRestart.desktop
 rm -f /opt/KernelPostRestart.sh
 rm -Rf /opt/KernelUpdate
-sed -i 's/$CurrentKernelFull/4.16.16-041616-generic/g' /opt/essentials/unsyncedupdates.html
+sed -i 's/$CurrentKernelFull/$KERNELNAME/g' /opt/essentials/unsyncedupdates.html
 notify-send -t 5000 \"PrathamOS Kernel Update\" \"\nUpdating Grub...\"
 xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /opt/anapmi/AI.png	
 mpv /opt/anapmi/updgrub.mp3
@@ -167,8 +87,8 @@ rm -f /opt/KERNELUPDATE
 exit
 
 " | tee /opt/KernelPostRestart.sh
-sudo chmod 777 /opt/KernelPostRestart.sh
-echo "[Desktop Entry]
+			sudo chmod 777 /opt/KernelPostRestart.sh
+			echo "[Desktop Entry]
 Encoding=UTF-8
 Version=0.9.4
 Type=Application
@@ -180,13 +100,33 @@ StartupNotify=false
 Terminal=false
 Hidden=false
 " | tee /home/$CurrentUser/.config/autostart/KernelPostRestart.desktop
-	notify-send -t 5000 "PrathamOS Kernel Update" "\nRestarting The System..."
-	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /opt/anapmi/AI.png	
-	mpv /opt/anapmi/restart2.mp3
-	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
-	echo "Restarting The System..."
-	sudo update-grub
-	xfce4-session-logout -r
+			notify-send -t 5000 "PrathamOS Kernel Update" "\nRestarting The System..."
+			xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /opt/anapmi/AI.png	
+			mpv /opt/anapmi/restart2.mp3
+			xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
+			echo "Restarting The System..."
+			sudo update-grub
+			xfce4-session-logout -r
+		else
+			notify-send -t 5000 "PrathamOS Kernel Update" "\nUnable To Connect Repository.\nPlease Try Again Later..."
+			xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /opt/anapmi/AI.png	
+			mpv /opt/anapmi/norepo.mp3
+			xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
+			echo "Unable To Connect Repository.Please Try Again Later."
+			rm -Rf /opt/KernelUpdate
+			rm -f /opt/KERNELUPDATE
+			exit
+		fi
+	else
+		notify-send -t 5000 "PrathamOS Kernel Update" "\nUnable To Connect Repository.\nPlease Try Again Later..."
+		xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /opt/anapmi/AI.png	
+		mpv /opt/anapmi/norepo.mp3
+		xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /usr/share/backgrounds/xfce/PrathamOS.png
+		echo "Unable To Connect Repository.Please Try Again Later."
+		rm -Rf /opt/KernelUpdate
+		rm -f /opt/KERNELUPDATE
+		exit
+	fi
 else
 	notify-send -t 5000 "PrathamOS Kernel Update" "\nUnable To Connect Repository.\nPlease Try Again Later..."
 	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/workspace0/last-image --set /opt/anapmi/AI.png	
